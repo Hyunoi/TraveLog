f5c747a0c625be641e1876c71046dfe0c8230333
Hyunoi <nhe0622@gmail.com>
2025-07-11 16:45:34 +0900

    feat: user Role 속성 추가

--- src/main/java/com/example/travelog/domain/user/entity/User.java
+++ src/main/java/com/example/travelog/domain/user/entity/User.java
@@ -2,8 +2,10 @@ package com.example.travelog.domain.user.entity;
 
 import com.example.travelog.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,4 +16,14 @@ public class User extends BaseTimeEntity {
 
     private String password;
     private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(String email, String password, String nickname, Role role) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
 }
