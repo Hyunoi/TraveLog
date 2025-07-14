59f6f1cab717e3a77e718f04a49b91926440d0d9
Hyunoi <nhe0622@gmail.com>
2025-07-14 19:34:14 +0900

    feat: /user/login (로그인) 기능 구현

--- src/main/java/com/example/travelog/domain/user/controller/UserController.java
+++ src/main/java/com/example/travelog/domain/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.travelog.domain.user.controller;
 
+import com.example.travelog.domain.user.dto.request.UserLogInRequest;
 import com.example.travelog.domain.user.dto.request.UserSignUpRequest;
+import com.example.travelog.domain.user.dto.response.UserLoginResponse;
 import com.example.travelog.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +22,11 @@ public class UserController {
     public ResponseEntity<?> signUp(@Valid @RequestBody UserSignUpRequest request) {
         userService.signUp(request);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLoginResponse> logIn(@Valid @RequestBody UserLogInRequest request) {
+        UserLoginResponse response = userService.logIn(request);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/travelog/domain/user/service/UserService.java
+++ src/main/java/com/example/travelog/domain/user/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.travelog.domain.user.service;
 
+import com.example.travelog.domain.user.dto.request.UserLogInRequest;
 import com.example.travelog.domain.user.dto.request.UserSignUpRequest;
+import com.example.travelog.domain.user.dto.response.UserLoginResponse;
 import com.example.travelog.domain.user.entity.Role;
 import com.example.travelog.domain.user.entity.User;
 import com.example.travelog.domain.user.repository.UserRepository;
 import com.example.travelog.global.exception.CustomException;
 import com.example.travelog.global.exception.ErrorCode;
+import com.example.travelog.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRedisService userRedisService;
 
     public void signUp(UserSignUpRequest request) {
         if (userRepository.existsUserByEmail(request.email()))
@@ -31,5 +36,21 @@ public class UserService {
                 Role.USER);
 
         userRepository.save(user);
+    }
+
+    public UserLoginResponse logIn(UserLogInRequest request) {
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUNT_USER));
+
+        if (!passwordEncoder.matches(request.password(), user.getPassword()))
+            throw new CustomException(ErrorCode.INVALID_PASSWORD);
+
+        String accessToken = jwtTokenProvider.createAccessToken(request.email());
+        String refreshToken = jwtTokenProvider.createRefreshToken(request.email());
+
+        userRedisService.setRefreshToken(request.email(), refreshToken);
+
+        return new UserLoginResponse(accessToken, refreshToken);
+
     }
 }
