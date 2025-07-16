3296a1d5a1bdf5de66438a225cf053693890f5cb
Hyunoi <nhe0622@gmail.com>
2025-07-16 15:42:13 +0900

    feat: Swagger 어노테이션 추가

--- src/main/java/com/example/travelog/domain/user/controller/UserController.java
+++ src/main/java/com/example/travelog/domain/user/controller/UserController.java
@@ -1,63 +1,42 @@
 package com.example.travelog.domain.user.controller;
 
-import com.example.travelog.domain.s3.service.S3ImageService;
 import com.example.travelog.domain.user.dto.request.UserLogInRequest;
 import com.example.travelog.domain.user.dto.request.UserProfileRequest;
 import com.example.travelog.domain.user.dto.request.UserSignUpRequest;
 import com.example.travelog.domain.user.dto.response.UserLoginResponse;
 import com.example.travelog.domain.user.dto.response.UserMyPageResponse;
-import com.example.travelog.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
-@RequestMapping("/api/v1/user")
-@RequiredArgsConstructor
-public class UserController {
-    private final UserService userService;
-    private final S3ImageService s3ImageService;
+@Tag(name = "USER")
+public interface UserController {
+    @Operation(summary = "회원가입")
+    public ResponseEntity<?> signUp(@Parameter(description = "이메일, 비밀번호, 닉네임 기입")
+                                    @Valid @RequestBody UserSignUpRequest request);
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@Valid @RequestBody UserSignUpRequest request) {
-        userService.signUp(request);
-        return ResponseEntity.ok().build();
-    }
+    @Operation(summary = "로그인")
+    public ResponseEntity<UserLoginResponse> logIn(@Parameter(description = "이메일, 비밀번호 기입")
+                                                   @Valid @RequestBody UserLogInRequest request);
 
-    @PostMapping("/login")
-    public ResponseEntity<UserLoginResponse> logIn(@Valid @RequestBody UserLogInRequest request) {
-        UserLoginResponse response = userService.logIn(request);
-        return ResponseEntity.ok(response);
-    }
+    @Operation(summary = "로그아웃")
+    public ResponseEntity<?> logOut(@AuthenticationPrincipal UserDetails userDetails);
 
-    @PostMapping("/logout")
-    public ResponseEntity<?> logOut(@AuthenticationPrincipal UserDetails userDetails) {
-        userService.logOut(userDetails.getUsername());
-        return ResponseEntity.ok().build();
-    }
+    @Operation(summary = "[마이페이지] 회원 정보 조회")
+    public ResponseEntity<UserMyPageResponse> getMyPage(@AuthenticationPrincipal UserDetails userDetails);
 
-    @GetMapping("/mypage")
-    public ResponseEntity<UserMyPageResponse> getMyPage(@AuthenticationPrincipal UserDetails userDetails) {
-        UserMyPageResponse response = userService.getMyPage(userDetails.getUsername());
-        return ResponseEntity.ok(response);
-    }
+    @Operation(summary = "[마이페이지] 회원 정보 수정")
+    public ResponseEntity<?> updateProfile(@Parameter(description = "닉네임 기입")
+                                           @RequestBody UserProfileRequest request,
+                                           @AuthenticationPrincipal UserDetails userDetails);
 
-    @PatchMapping("/profile")
-    public ResponseEntity<?> updateProfile(@RequestBody UserProfileRequest request,
-                                           @AuthenticationPrincipal UserDetails userDetails) {
-        userService.updateProfile(request, userDetails.getUsername());
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/profile-image")
+    @Operation(summary = "[마이페이지] 프로필 사진 등록/수정")
     public String updateProfileImage(@RequestPart("image") MultipartFile image,
-                                   @AuthenticationPrincipal UserDetails userDetails) {
-        String imageUrl = s3ImageService.upload(image);
-        userService.updateProfileImage(imageUrl, userDetails.getUsername());
-        return imageUrl;
-    }
+                                     @AuthenticationPrincipal UserDetails userDetails);
 }
