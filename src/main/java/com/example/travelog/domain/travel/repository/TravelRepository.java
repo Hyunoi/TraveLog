54fa6bfab84d8384e5f712ea62e9dfee9ef5a793
Hyunoi <nhe0622@gmail.com>
2025-07-18 17:01:04 +0900

    feat: /travel (여행 생성) 메소드 생성

---
+++ src/main/java/com/example/travelog/domain/travel/repository/TravelRepository.java
@@ -0,0 +1,10 @@
+package com.example.travelog.domain.travel.repository;
+
+import com.example.travelog.domain.travel.entity.Travel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TravelRepository extends JpaRepository <Travel, Long> {
+
+}
--- src/main/java/com/example/travelog/domain/travel/service/TravelService.java
+++ src/main/java/com/example/travelog/domain/travel/service/TravelService.java
@@ -1,7 +1,38 @@
 package com.example.travelog.domain.travel.service;
 
+import com.example.travelog.domain.travel.dto.request.TravelCreateRequest;
+import com.example.travelog.domain.travel.dto.request.TravelUpdateRequest;
+import com.example.travelog.domain.travel.entity.Travel;
+import com.example.travelog.domain.travel.repository.TravelRepository;
+import com.example.travelog.domain.user.entity.User;
+import com.example.travelog.domain.user.repository.UserRepository;
+import com.example.travelog.global.exception.CustomException;
+import com.example.travelog.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TravelService {
 }
+    private final UserRepository userRepository;
+    private final TravelRepository travelRepository;
+
+    public void createTravel(TravelCreateRequest request, String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUNT_USER));
+
+        Travel travel = Travel.builder()
+                .user(user)
+                .title(request.title())
+                .description(request.description())
+                .location(request.location())
+                .startTime(request.startDate())
+                .endTime(request.endDate())
+                .latitude(request.latitude())
+                .longitude(request.longitude())
+                .build();
+
+        travelRepository.save(travel);
+    }
